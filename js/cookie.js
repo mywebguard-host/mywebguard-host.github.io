39f54e228b4ea7759a1c22fc5e55ae1a856b67de
Phu Phung <pphung1@udayton.edu>
2019-08-12 13:44:17 -0400

    remove debug

--- js/cookie.js
+++ js/cookie.js
@@ -1,5 +1,5 @@
 if (document.cookie && document.cookie.indexOf("mywebguard")>=0) {
-	alert("Debug from https://mywebguard-host.github.io: document.cookie="+
-			document.cookie);
+	//alert("Debug from https://mywebguard-host.github.io: document.cookie="+
+	//		document.cookie);
 }else
 	document.cookie="mywebguard=mywebguard-cookie-test";
