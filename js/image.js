8f9a61a1fe245dc849589904f5d95e967260faa0
Phu Phung <pphung1@udayton.edu>
2019-08-13 00:17:11 -0400

    Test Image()

--- js/image.js
+++ js/image.js
@@ -1,4 +1,9 @@
-var myImage = new Image(100, 200);
-myImage.src = 'https://homepages.cae.wisc.edu/~ece533/images/tulips.png';
-document.body.appendChild(myImage);
+var imgObject = new Image(200);
+imgObject.src = 'https://homepages.cae.wisc.edu/~ece533/images/tulips.png';
+document.body.appendChild(imgObject);
+
+var imgElement = document.createElement("img");
+imgElement.src = "https://homepages.cae.wisc.edu/~ece533/images/peppers.png";
+document.body.appendChild(imgElement);
+
 
